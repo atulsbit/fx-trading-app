28ac3abc9c14deb9e1b42479fbf64b2d59d039e5
Atul Shukla <atul.shukla@gmail.com>
2021-09-17 00:45:52 -0400

    register page component added

--- ui/src/app/pages/register-page/register-page.component.ts
+++ ui/src/app/pages/register-page/register-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register-page',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    })
+  }
+
+  // Convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // Exit function if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.toastr.success('Registration successful!');
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.toastr.error(error);
+          this.loading = false;
+        }
+      )
   }
 
 }
